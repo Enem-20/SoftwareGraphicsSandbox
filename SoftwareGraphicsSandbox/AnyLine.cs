02c1311474764646cf0f49de7b21500a5c7337dd
Enem-20 <57913763+Enem-20@users.noreply.github.com>
2022-03-05 19:07:29 +0300

    continue my research

---
+++ SoftwareGraphicsSandbox/AnyLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace UserSoftwareGraphicsSandbox{
+    class AnyLine : PrimaryRenderObject{
+        public AnyLine(int x1 = 0, int y1 = 0, int x2 = 0, int y2 = 0) : base(new int[4] {x1, y1, x2, y2}){}
+        public AnyLine(int[] points){
+            SetArgs(points);
+        }
+
+    }
+}
---
+++ SoftwareGraphicsSandbox/AnyPoint.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserSoftwareGraphicsSandbox{
+    class AnyPoint : PrimaryRenderObject{
+        public AnyPoint(int x, int y) : base(x, y) { }
+    }
+}
